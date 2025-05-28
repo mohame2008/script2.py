ced75e8e6c9a3d6969145a85b140fe989a1e81e3
mohame2008 <oulidimohamedsolaimane@gmail.com>
2025-05-28 19:51:40 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,7 +2,8 @@ import requests
 from rich import print
 
 def update_script():
-    url='file:///home/mohamed/%D8%B3%D8%B7%D8%AD%20%D8%A7%D9%84%D9%85%D9%83%D8%AA%D8%A8/script2.py'
+    url='https://raw.githubusercontent.com/mohame2008/script2.py/387180260a1af50e3a470c7656963885763596b1/script2.py'
+
     response= requests.get(url)
     if response.status_code==200: 
 
@@ -12,8 +13,8 @@ def update_script():
         print("update to the new version")
     else:
         print("no updates found")    
-
-
+print("-----------------------")
+print("-----------------------")
 print("-----------------------")
 update_script()
 print("-----------------------")
@@ -21,3 +22,6 @@ print("[yellow]welcom [/]")
 print("-----------------------")
 name=input("enter ur name : ")
 print("your name is : ",name)
+print("-----------------------")
+
+print("-----------------------")
