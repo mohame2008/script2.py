c29314719ae31ef23bcb9309d6a4e136b8cf1024
mohame2008 <oulidimohamedsolaimane@gmail.com>
2025-05-28 19:15:35 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,7 +2,7 @@ import requests
 from rich import print
 
 def update_script():
-    url='https://github.com/mohame2008/script2.py/blob/main/script2.py'
+    url='aw.githubusercontent.com/mohame2008/script2.py/341457a525c5e5e0d9147e77c30574d2b01306da/script2.py'
     response= requests.get(url)
     if response.status_code==200: 
         print("update to the new version")
